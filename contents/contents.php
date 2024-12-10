268175817fadead49a18ea1a3e0639832b4f4d54
leojoegem <leojoegem@gmail.com>
2024-12-10 09:34:36 +0300

    re-structured files

--- contents/contents.php
+++ contents/contents.php
@@ -2,7 +2,7 @@
 class contents{
     public function index_content() {
         ?>
-        <h1>Index Content</h1>
-        <?php   
+        <h1>Hello World!!</h1>
+        Today is <?php print date(1);
     }
 }
--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 
 //importing content from an external file
-require_once 'classes/layout.php';
-require_once 'content/layout.php';
+require_once 'AutoLoad.php';
 
-//creating an instance of a class
-$ObjLayout = new layout();
-$ObjContent = new contents();
+
 
 //calling a method from a class/invoking a method
 
