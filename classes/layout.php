81d6ebffb5ebf6321efc5211188b57d3c3aa40e2
leojoegem <leojoegem@gmail.com>
2024-12-10 09:43:53 +0300

    added bootstrap

--- classes/layout.php
+++ classes/layout.php
@@ -2,20 +2,20 @@
 class layout{
     public function heading(){
         ?>
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
         <?php
     }
     public function footer(){
         ?>
         </body>
-        </html>
-        <?php   
+    </html>
+        <?php
     }
 }
--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
+// Importing content from an external file
+require_once "AutoLoad.php";
 
-//importing content from an external file
-require_once 'AutoLoad.php';
-
-
-
-//calling a method from a class/invoking a method
-
+// Invoke a method
 $ObjLayout->heading();
 $ObjContent->index_content();
 $ObjLayout->footer();
-
