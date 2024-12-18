182ebb3a84ba2a583d0b1f08b3be2279eebb2152
leojoegem <leojoegem@gmail.com>
2024-12-18 14:15:49 +0300

    added search bar

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<?php
-// Importing content from an external file
-require_once "AutoLoad.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API Home</title>
+    <link href = "https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel = "stylesheet" integrity = "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E2633wXjSkEh2U6UHDtUy4UJfjG" crossorigin = "anonymous">
+</head>
+<body>
+    <form action = "" method = "post">
+        <input class="form-control form-control-lg" id="search" type="text" name="search" placeholder="Search..." autofocus>
+    </form>
 
-// Invoke a method
-$ObjLayout->heading();
-$ObjContent->index_content();
-$ObjLayout->footer();
+<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src = "userdisplay.js"></script>
+</body>
+</html>
