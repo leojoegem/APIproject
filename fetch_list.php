31645d9b7485e55aeafe2c587590c42ae2a22d81
leojoegem <leojoegem@gmail.com>
2024-12-18 14:35:59 +0300

    search bar working kind of

--- fetch_list.php
+++ fetch_list.php
@@ -2,13 +2,49 @@
 // fetch_list.php
 require_once 'dbConnect.php';
 
-$sql = "SELECT * FROM Motorcycle";
+if(isset($_POST['search_term'])) {
+    $search_term = $_POST['search_term'];
+    $sql = "SELECT * FROM Motorcycle WHERE make LIKE '%$search_term%' OR model LIKE '%$search_term%' OR year LIKE '%$search_term%' OR price LIKE '%$search_term%'";
+} else {
+    $sql = "SELECT * FROM Motorcycle";
+}
 
 $stmt = $conn->query($sql);
 
 $motorcycle = $stmt->fetch_all(MYSQLI_ASSOC);
-
-foreach ($motorcycle as $motorcycle) {
-    echo $motorcycle['make'] . '<br>';
-}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Motorcycle List</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <h2 class="mb-4">Motorcycle List</h2>
+        <table class="table table-striped table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Make</th>
+                    <th>Model</th>
+                    <th>Year</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($motorcycle as $motorcycle) : ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($motorcycle['make']); ?></td>
+                        <td><?php echo htmlspecialchars($motorcycle['model']); ?></td>
+                        <td><?php echo htmlspecialchars($motorcycle['year']); ?></td>
+                        <td><?php echo htmlspecialchars($motorcycle['price']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,10 +6,26 @@
     <title>API Home</title>
     <link href = "https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel = "stylesheet" integrity = "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E2633wXjSkEh2U6UHDtUy4UJfjG" crossorigin = "anonymous">
 </head>
+
+<style>
+    #search {
+        margin-top: 20px;
+        margin-left: 20px;
+        width: 500px;
+    }
+</style>
 <body>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $input = htmlspecialchars($_POST['in_put']);
+        echo "<div id='out_put'>You searched for: $input</div>";
+    }
+    ?>
     <form action = "" method = "post">
-        <input class="form-control form-control-lg" id="search" type="text" name="search" placeholder="Search..." autofocus>
+        <input class="form-control form-control-lg" type="text" name="in_put" id="in_put" placeholder="Search..." autofocus>
     </form>
+    <br>
+    <div id = "out_put"></div>
 
 <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src = "userdisplay.js"></script>
